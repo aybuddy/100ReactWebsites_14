fcd072f6b1490f8c53c078da1f10452e1cf0c170
Aaron Yu <ayybuddyy@gmail.com>
2021-03-04 17:04:44 -0500

    Connect to firebase for db

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Video from './Video';
+import db from './firebase';
 
 import './App.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection('videos').onSnapshot((snapshot) =>
+      setVideos([snapshot.docs.map((doc) => doc.data())])
+    );
+  }, []);
+
   return (
     <div className='app'>
       <div className='app__videos'>
-        <Video
-          url='https://v19-us.tiktokcdn.com/68df685953544fb829c8a778ca63e120/60419b04/video/tos/useast2a/tos-useast2a-ve-0068c002/ea4bb8c34fd1429f961f07af54d26b35/?a=1233&br=2880&bt=1440&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2021030420440401019019213119465D72&lr=tiktok_m&mime_type=video_mp4&pl=0&qs=0&rc=amlsZXMzZm1yMzMzOTczM0ApOGQzOTZnZWVmNzNlMzs3aGdxcy5qNl9mcWRgLS0wMTZzc2E2LzQyNTYyXzBgM18yMjI6Yw%3D%3D&vl=&vr='
-          // channel=''
-          // description=''
-          // song=''
-          // likes=''
-          // message=''
-          // shares=''
-        />
-        <Video />
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBPbjP-7pCEzfJHvfCdBDYXMvHgK0hYhTw',
   authDomain: 'tiktok-clone-cp.firebaseapp.com',
@@ -7,3 +9,9 @@ const firebaseConfig = {
   appId: '1:151267792801:web:586a38d67a8fcba6c39de0',
   measurementId: 'G-K53W8T0HH5',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
